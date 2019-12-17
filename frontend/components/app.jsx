e200097f17090b4fd2d4b961ecc34f12f700cb33
Nick S <nickjsaulsberry@outlook.com>
2019-12-16 19:01:29 -0600

    Completed sessions Reducer and util methods for backend auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import GreetingContainer from "./greeting/greeting_container";
+import SignUpFormContainer from "./session_form/signup_form_container";
+import LogInFormContainer from "./session_form/login_form_container";
+import NavBar from './navbar/navbar';
+import { Switch } from 'react-router-dom';
 
 const App = () => (
-    <div>
-        <h1>Hello World</h1>
-    </div>
-)
+  <div>
+    <NavBar />
+    <Switch>
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
+    <h1>Hello World</h1>
+  </div>
+);
 
 export default App;
