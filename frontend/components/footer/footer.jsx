1f732b6bd3ec244357c8e163b914f0f359e33391
Nick S <nickjsaulsberry@outlook.com>
2020-02-27 17:33:19 -0600

    styled autocomplete and added demo login for user

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-const Footer = () => (
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+const Footer = props => (
         <ul className="footer">
             <li><p className="copyright"> &copy;2019 Haply</p></li>
             <div className="footer-center">
                 <li><a href="https://github.com/passsionforprogramming/eventbrite/wiki/MVP-List">MVP List</a></li>
                 <li><a href="https://github.com/passsionforprogramming/eventbrite/wiki/Schema">Schema</a></li>
-                <li><a href="#">Demo Login</a></li>
-                <li><a href="#">About Nick Saulsberry</a></li>
+                <li onClick={() => {
+                    const user = {
+                        email: "barryallen@haply.com",
+                        password: "123456"
+                    };
+                    props.login(user);
+                }}>Demo Login</li>
+                <li><a href="#">About James Saulsberry</a></li>
             </div>
         </ul>
 );
 
-export default Footer;
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+    login: user => dispatch(login(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
--- frontend/components/search_bar/auto_complete_list.jsx
+++ frontend/components/search_bar/auto_complete_list.jsx
@@ -5,6 +5,7 @@ const AutoCompleteList = props => {
     const { events } = props;
     return (
         <div className="auto-complete-list">
+            <p className="evt-search-header">Events</p>
             {
                 events.map(event => (
                     <EventListItem 
