275421ca44d24f080842a416ba2d808dbcbdcac6
Nick S <nickjsaulsberry@outlook.com>
2020-03-20 15:04:10 -0500

    updated event map with maps center on url params

--- frontend/components/EventMap/EventMap.jsx
+++ frontend/components/EventMap/EventMap.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class EventMap extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            mapOptions: {
+                center: {
+                    lat: this.props.match.params.lat,
+                    lng: this.props.atch.params.lon
+                },
+                zoom: 13
+            }
+        }
+    }
     render(){
         return (
             <div className="map" ref="map">
