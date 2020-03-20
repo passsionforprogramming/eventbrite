300aad7d65964b4efeeb037b700e112d69ce9881
Nick S <nickjsaulsberry@outlook.com>
2020-03-20 14:52:48 -0500

    fixed event map

---
+++ frontend/components/EventMap/EventMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class EventMap extends React.Component {
+
+    render(){
+        return (
+            <div className="map" ref="map">
+
+            </div>
+        )
+    }
+}
